f38619969a8b15db3636131bfa4e549c11b4dda6
Payam <p.hoseini.ch@gmail.com>
2024-02-23 20:26:41 +0330

    Install Mongoose and connect to our Mongodb database with that.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -21,4 +24,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
