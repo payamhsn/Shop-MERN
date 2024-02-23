50103fcfc48f9123e196445660c6208d6eac5c4b
Payam <p.hoseini.ch@gmail.com>
2024-02-23 18:15:49 +0330

    This is a sample how to get a params from url using react router dom and use it to get data from local json file OR use it to get specified data from DB.

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import products from "../products";
+
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+
+  const product = products.find((p) => p._id === productId);
+
+  console.log(productId);
+  console.log(product);
+
+  return <div>ProductScreen</div>;
+};
+
+export default ProductScreen;
