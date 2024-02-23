6b91f63cf7de1a5f97c54bc62c606efc8da62afd
Payam <p.hoseini.ch@gmail.com>
2024-02-23 18:55:00 +0330

    A good example of defining routes for backend. also a good example of how to get something from url (product id) and use that.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
