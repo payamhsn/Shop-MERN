4d31457a74c9c7f849c8df267eeb36353b8deca5
Payam <p.hoseini.ch@gmail.com>
2024-02-24 16:49:59 +0330

    Create my custom error middleware and use it in server file that i can use that in all of my app.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.use("/api/products", productRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
