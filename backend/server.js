0412a0f6e1181410d33296373bcd08dea01f44c7
Payam <p.hoseini.ch@gmail.com>
2024-02-24 15:34:18 +0330

    A sample of using Express router which we can use in a file for defining routes there and keep main server file clean.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -11,14 +11,7 @@ connectDB();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
