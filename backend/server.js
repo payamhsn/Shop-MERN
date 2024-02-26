0a686e28e508c6eab2458e227762c22d77a22f5a
Payam <p.hoseini.ch@gmail.com>
2024-02-26 21:55:50 +0330

    Create and define all the user routes. Create user controller code base config.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
