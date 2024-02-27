e5855d40c3fb55ed7b368b7a6b476b9dc0915f32
Payam <p.hoseini.ch@gmail.com>
2024-02-27 21:00:53 +0330

    Create login page ui and make its route.

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = () => {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email"></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          <Link>Register</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
