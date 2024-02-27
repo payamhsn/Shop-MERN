8b5209925da2deeca0cb7425a4f10feab4993352
Payam <p.hoseini.ch@gmail.com>
2024-02-27 18:04:13 +0330

    Create auth middleware which decode token from req and take user id from that and specify user.Based on user now we can protect routes for users and admins.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
